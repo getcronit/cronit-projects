e31c2fbfd9cb6fb1f4efe1fd86452015e360118d
Jan-Emig <jan.emig@pm.me>
2024-11-15 16:09:31 +0100

    Action CREATE: ./src/carService.ts
    Purpose: To implement a new car service for managing car data.

--- d47953ce-889a-4c12-86dd-0900536e47c2/src/carService.ts
+++ d47953ce-889a-4c12-86dd-0900536e47c2/src/carService.ts
@@ -1,43 +1,19 @@
-interface Car {
-  id: number;
-  make: string;
-  model: string;
-  year: number;
-}
+export class CarService {
+  private cars: { id: number; model: string; brand: string }[] = [];
 
-class CarService {
-  private cars: Car[] = [];
-  private nextId: number = 1;
+  addCar(id: number, model: string, brand: string) {
+    this.cars.push({ id, model, brand });
+  }
 
-  listCars(): Car[] {
+  removeCar(id: number) {
+    this.cars = this.cars.filter(car => car.id !== id);
+  }
+
+  listCars() {
     return this.cars;
   }
 
-  $createCar(make: string, model: string, year: number): Car {
-    const newCar: Car = { id: this.nextId++, make, model, year };
-    this.cars.push(newCar);
-    return newCar;
-  }
-
-  $updateCar(id: number, make: string, model: string, year: number): Car | null {
-    const car = this.cars.find(car => car.id === id);
-    if (car) {
-      car.make = make;
-      car.model = model;
-      car.year = year;
-      return car;
-    }
-    return null;
-  }
-
-  $deleteCar(id: number): boolean {
-    const index = this.cars.findIndex(car => car.id === id);
-    if (index !== -1) {
-      this.cars.splice(index, 1);
-      return true;
-    }
-    return false;
+  findCarById(id: number) {
+    return this.cars.find(car => car.id === id);
   }
 }
-
-export default new CarService();
