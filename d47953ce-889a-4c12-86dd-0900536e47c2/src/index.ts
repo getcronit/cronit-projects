0aa7d13aaa6f5d57ff658aae3770a50d03842da0
Jan-Emig <jan.emig@pm.me>
2024-11-15 14:18:57 +0100

    Action UPDATE: ./src/index.ts
    Purpose: Remove console.log statements from the index.ts file.

--- d47953ce-889a-4c12-86dd-0900536e47c2/src/index.ts
+++ d47953ce-889a-4c12-86dd-0900536e47c2/src/index.ts
@@ -4,25 +4,20 @@ import userService from './userService';
 export const graphql = {
   Query: {
     hello: () => {
-      console.log('Query: hello called');
       return 'Hello, world!';
     },
     users: () => {
-      console.log('Query: users called');
       return userService.listUsers();
     }
   },
   Mutation: {
     createUser: (name: string, email: string) => {
-      console.log('Mutation: createUser called with', { name, email });
       return userService.$createUser(name, email);
     },
     updateUser: (id: number, name: string, email: string) => {
-      console.log('Mutation: updateUser called with', { id, name, email });
       return userService.$updateUser(id, name, email);
     },
     deleteUser: (id: number) => {
-      console.log('Mutation: deleteUser called with', { id });
       return userService.$deleteUser(id);
     }
   }
