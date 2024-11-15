6d7036f5d18e902841ca4114ebb647fe642cd650
Jan-Emig <jan.emig@pm.me>
2024-11-15 17:05:14 +0100

    Action UPDATE: ./src/index.ts
    Purpose: Integrate the new UserService into the GraphQL API

--- d47953ce-889a-4c12-86dd-0900536e47c2/src/index.ts
+++ d47953ce-889a-4c12-86dd-0900536e47c2/src/index.ts
@@ -1,13 +1,23 @@
 import { app } from '@getcronit/pylon';
+import { UserService } from './userService';
+
+const userService = new UserService();
 
 export const graphql = {
   Query: {
     hello: () => {
       console.log('Hello query was called');
       return 'Hello, world!';
+    },
+    listUsers: () => {
+      return userService.listUsers();
     }
   },
-  Mutation: {}
+  Mutation: {
+    addUser: (name: string) => {
+      return userService.addUser(name);
+    }
+  }
 };
 
 export default app;
